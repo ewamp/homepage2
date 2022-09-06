9158c7e3d769dd6884aa12ce83d160f2e520cc1b
Ewa Madej-Pawlowska <ewa.madej@gmail.com>
2022-09-06 23:43:57 +0200

    classes changed

--- js/script.js
+++ js/script.js
@@ -5,18 +5,18 @@
 
     const togglePictureSize = () => {
         const section__trailPicture = document.querySelector(".js-section__trailPicture");
-        const changeSpanText = document.querySelector(".js-changeSpanText");
+        const spanText = document.querySelector(".js-spanText");
 
         section__trailPicture.classList.toggle("section__trailPicture--bigSize");
-        changeSpanText.innerText = section__trailPicture.classList.contains("section__trailPicture--bigSize") ? "Zmniejsz" : "Powiększ";
+        spanText.innerText = section__trailPicture.classList.contains("section__trailPicture--bigSize") ? "Zmniejsz" : "Powiększ";
     };
 
     const togglePictureSize2 = () => {
         const section__trailPicture2 = document.querySelector(".js-section__trailPicture2");
-        const changeSpanText2 = document.querySelector(".js-changeSpanText2");
+        const spanText2 = document.querySelector(".js-spanText2");
 
         section__trailPicture2.classList.toggle("section__trailPicture--bigSize");
-        changeSpanText2.innerText = section__trailPicture2.classList.contains("section__trailPicture--bigSize") ? "Zmniejsz" : "Powiększ";
+        spanText2.innerText = section__trailPicture2.classList.contains("section__trailPicture--bigSize") ? "Zmniejsz" : "Powiększ";
     };
 
     const init = () => {
